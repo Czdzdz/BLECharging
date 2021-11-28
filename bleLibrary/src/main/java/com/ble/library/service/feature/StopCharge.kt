dfa142b2dc3b7c05223dfcf251462cf303faf356
yaoyihang <345709209@qq.com>
2021-11-28 13:53:06 +0800

    重写StopCharge

--- bleLibrary/src/main/java/com/ble/library/service/feature/StopCharge.kt
+++ bleLibrary/src/main/java/com/ble/library/service/feature/StopCharge.kt
@@ -1,9 +1,9 @@
 package com.ble.library.service.feature
 
+import android.util.Log
 import com.ble.library.service.BLEDiagram
 import com.ble.library.service.BLEDiagramJson
 import com.ble.library.service.CMD_END_CHARGE
-import com.ble.library.service.DataMaker
 
 /**
  * 结束充电
@@ -14,7 +14,7 @@ class StopChargeData(override var hexCode: Byte) : BLEDiagramJson {
         hexCode = CMD_END_CHARGE
     }
 
-    var orderId: String = ""
+    var orderId: String = "123456789011"
 }
 
 class StopCharge(override var hexCode: Byte) : BLEDiagram {
@@ -24,9 +24,27 @@ class StopCharge(override var hexCode: Byte) : BLEDiagram {
         return if (stopChargeData == null) {
             null
         } else {
-            val maker = DataMaker()
-            maker.append(json.orderId, 6)
-            maker.final()
+            var orderNum = json.orderId
+            var payload_bytes = IntArray(8)
+            Log.e("fuck", "orderNum:$orderNum")
+            var p = 0
+            var i = 11
+            while (i > 0) {
+                val a = orderNum.substring(i, i + 1).toByte()
+                val b = orderNum.substring(i - 1, i).toByte()
+                Log.e("fuck", "a=$a,b=$b,运算:${(b * 16 + a)}")
+                payload_bytes[p] = b * 16 + a
+                p++
+                i -= 2
+            }
+            for (i in payload_bytes.indices) {
+                Log.e("fuck", "数组的值:" + payload_bytes[i])
+            }
+            val data = ByteArray(payload_bytes.size) { payload_bytes[it].toByte() }
+            data
+//            val maker = DataMaker()
+//            maker.append(json.orderId, 6)
+//            maker.final()
         }
     }
 
